bd2c531364550b7097ef056a84ff1481e178f23b
Atte-Oksanen <atteoksanen@outlook.com>
2023-08-20 14:42:49 +0300

    finished 2.17

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -24,11 +24,41 @@ const PersonForm = (props) => {
   )
 }
 
+const Notification = ({message, error}) => {
+  if(message === null){
+    return null
+  }
+  let style
+  if(error !== true){
+    style = {
+      color: 'Green',
+      border: '1px solid black',
+      backgroundColor: '#DADADA',
+      fontSize: '18px'
+    }
+  } else {
+    style = {
+      color: 'Red',
+      border: '1px solid black',
+      backgroundColor: '#DADADA',
+      fontSize: '18px'
+    }
+  }
+
+  return (
+    <div style={style}>
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchBar, setSearch] = useState('')
+  const [statusMessage, setMessage] = useState(null)
+  const [messageError, setError] = useState(false)
 
   const toShow = persons.filter(person => person.name.toLowerCase().includes(searchBar.toLowerCase()))
 
@@ -47,28 +77,58 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleMessage = message => {
+    setMessage(message)
+    setTimeout(()=> {
+      setMessage(null)
+      setError(false)
+    }, 3000)
+  }
+
   const handleSetPersons = (event) => {
     event.preventDefault()
     if (persons.map(person => person.name).includes(newName)) {
       if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
         const changedNumber = { ...persons.find(person => person.name.includes(newName)), number: newNumber }
-        dbService.put(changedNumber).then(() => dbService.getData().then(data => setPersons(data)))
+        dbService.put(changedNumber)
+        .then(() => {
+          dbService.getData().then(data => setPersons(data))
+          handleMessage(`Updated ${changedNumber.name}'s number to ${changedNumber.number}`)
+        })
+        .catch(() => {
+          setError(true)
+          handleMessage(`${changedNumber.name} has already been deleted`)
+          dbService.getData().then(data => setPersons(data))
+        })
       }
     }
     else {
       const person = { name: newName, number: newNumber }
-      dbService.create(person).then(newPerson => setPersons(persons.concat(newPerson)))
+      dbService.create(person).then(newPerson => {
+        setPersons(persons.concat(newPerson))
+        handleMessage(`Added ${newPerson.name}`)
+      })
     }
   }
 
   const handleDelete = (person) => {
     if (window.confirm(`Delete ${person.name}?`)) {
-      dbService.remove(person.id).then(() => dbService.getData().then(data => setPersons(data)))
+      dbService.remove(person.id)
+      .then(() => {
+        dbService.getData().then(data => setPersons(data))
+        handleMessage(`Deleted ${person.name}`)
+      })
+      .catch(() => {
+        setError(true)
+        handleMessage(`${person.name} has already been deleted`)
+        dbService.getData().then(data => setPersons(data))
+      })
     }
   }
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={statusMessage} error={messageError} />
       <SearchBar handler={handleSearch} />
       <h3>add a new</h3>
       <PersonForm nameHandler={handleNewName} numberHandler={handleNewNumber} personHandler={handleSetPersons} />
