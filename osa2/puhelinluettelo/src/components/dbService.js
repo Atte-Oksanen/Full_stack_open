f1db8e3e608b49941ad02a1c824ff9a38ec0f1c8
Atte-Oksanen <atteoksanen@outlook.com>
2023-09-06 14:19:27 +0300

    updated dbService urls

--- osa2/puhelinluettelo/src/components/dbService.js
+++ osa2/puhelinluettelo/src/components/dbService.js
@@ -1,20 +1,20 @@
 import axios from "axios";
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'api/persons'
 
 const getData = () => {
     return axios.get(baseUrl).then(response => response.data)
 }
 
 const create = newDataPoint => {
-    return axios.post('http://localhost:3001/persons/', newDataPoint).then(response => (response.data))
+    return axios.post('/api/persons/', newDataPoint).then(response => (response.data))
 }
 
 const remove = id => {
-    return axios.delete(`http://localhost:3001/persons/${id}`).then(response => response.data)
+    return axios.delete(`/api/persons/${id}`).then(response => response.data)
 }
 
 const put = person => {
-    return axios.put(`http://localhost:3001/persons/${person.id}`, person).then(response => response.data)
+    return axios.put(`/api/persons/${person.id}`, person).then(response => response.data)
 }
 
 const exportFunctions = {
