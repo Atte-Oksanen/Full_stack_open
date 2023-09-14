1c5333c34ad1fe8162faa8d69487a012a90d1ae2
Atte-Oksanen <atteoksanen@outlook.com>
2023-09-14 10:49:30 +0300

    finished 4.1

---
+++ osa4/app.js
@@ -0,0 +1,23 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+require('dotenv').config()
+
+mongoose.connect(config.MONGO_URI)
+.then(() =>{
+    logger.info('connected to database')
+})
+.catch(error => {
+    logger.info(`Error connecting to database ${error.message}`)
+})
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
---
+++ osa4/controllers/blogs.js
@@ -0,0 +1,20 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogRouter.get('/', (req, res) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            res.json(blogs)
+        })
+})
+
+blogRouter.post('/', (req, res) => {
+    const blogPost = new Blog(req.body)
+
+    blogPost.save().then(result => {
+        res.status(201).json(result)
+    })
+})
+
+module.exports = blogRouter
